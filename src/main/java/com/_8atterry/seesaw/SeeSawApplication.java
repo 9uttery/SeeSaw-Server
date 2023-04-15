a744dec0faa133a3aedaaf46525c52a630295516
mingeun0507 <tqdlfkg@naver.com>
2023-04-15 17:27:18 +0900

    initial commit

---
+++ src/main/java/com/_8atterry/seesaw/SeeSawApplication.java
@@ -0,0 +1,13 @@
+package com._8atterry.seesaw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeeSawApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SeeSawApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/_8atterry/seesaw/SeeSawApplicationTests.java
@@ -0,0 +1,13 @@
+package com._8atterry.seesaw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeeSawApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
