d4a041d436e34c27f9c49629a55fc0ddaf289a27
mingeun0507 <tqdlfkg@naver.com>
2023-04-28 15:29:30 +0900

    Feat(#2-Error-control): 사용자 지정 에러 구현

---
+++ src/main/java/com/_8attery/seesaw/exception/InvalidRequestException.java
@@ -0,0 +1,7 @@
+package com._8attery.seesaw.exception;
+
+public class InvalidRequestException extends RuntimeException {
+    public InvalidRequestException(String message) {
+        super(message);
+    }
+}
