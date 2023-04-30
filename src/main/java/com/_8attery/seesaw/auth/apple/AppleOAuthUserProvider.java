c78c99203426801a143a257dd511bc1bffcc2eb4
mingeun0507 <tqdlfkg@naver.com>
2023-04-30 20:33:06 +0900

    Feat(#4-login): id_token에서 email을 추출해내는 OauthUserProvider 구현

---
+++ src/main/java/com/_8attery/seesaw/auth/apple/AppleOAuthUserProvider.java
@@ -0,0 +1,38 @@
+package com._8attery.seesaw.auth.apple;
+
+import com._8attery.seesaw.dto.auth.apple.ApplePublicKeys;
+import com._8attery.seesaw.exception.custom.InvalidTokenException;
+import io.jsonwebtoken.Claims;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+import java.security.PublicKey;
+import java.util.Map;
+
+@Component
+@RequiredArgsConstructor
+public class AppleOAuthUserProvider {
+
+    private final AppleJwtParser appleJwtParser;
+    private final AppleClient appleClient;
+    private final PublicKeyGenerator publicKeyGenerator;
+    private final AppleClaimsValidator appleClaimsValidator;
+
+    public String getApplePlatformMember(String identityToken) {
+        Map<String, String> headers = appleJwtParser.parseHeaders(identityToken);
+        ApplePublicKeys applePublicKeys = appleClient.getApplePublicKeys();
+
+        PublicKey publicKey = publicKeyGenerator.generatePublicKey(headers, applePublicKeys);
+
+        Claims claims = appleJwtParser.parsePublicKeyAndGetClaims(identityToken, publicKey);
+        validateClaims(claims);
+        return claims.get("email", String.class);
+    }
+
+    private void validateClaims(Claims claims) {
+        if (!appleClaimsValidator.isValid(claims)) {
+            throw new InvalidTokenException("Apple OAuth Claims 값이 올바르지 않습니다.");
+        }
+    }
+}
+
