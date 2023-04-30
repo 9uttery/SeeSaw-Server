bc9e00c84afe52283792d68f30f7df7bd2418eb9
mingeun0507 <tqdlfkg@naver.com>
2023-04-30 20:30:59 +0900

    Feat(#4-login): id_token을 파싱한 뒤, 생성한 공개키로 Claim을 가져오는 Parser 구현

---
+++ src/main/java/com/_8attery/seesaw/auth/apple/AppleJwtParser.java
@@ -0,0 +1,44 @@
+package com._8attery.seesaw.auth.apple;
+
+import com._8attery.seesaw.exception.custom.InvalidTokenException;
+import com._8attery.seesaw.exception.custom.TokenExpiredException;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import io.jsonwebtoken.*;
+import org.springframework.stereotype.Component;
+import org.springframework.util.Base64Utils;
+
+import java.security.PublicKey;
+import java.util.Map;
+
+@Component
+public class AppleJwtParser {
+
+    private static final String IDENTITY_TOKEN_VALUE_DELIMITER = "\\.";
+    private static final int HEADER_INDEX = 0;
+
+    private static final ObjectMapper OBJECT_MAPPER = new ObjectMapper();
+
+    public Map<String, String> parseHeaders(String identityToken) {
+        try {
+            String encodedHeader = identityToken.split(IDENTITY_TOKEN_VALUE_DELIMITER)[HEADER_INDEX];
+            String decodedHeader = new String(Base64Utils.decodeFromUrlSafeString(encodedHeader));
+            return OBJECT_MAPPER.readValue(decodedHeader, Map.class);
+        } catch (JsonProcessingException | ArrayIndexOutOfBoundsException e) {
+            throw new InvalidTokenException("Apple OAuth Identity Token 형식이 올바르지 않습니다.");
+        }
+    }
+
+    public Claims parsePublicKeyAndGetClaims(String idToken, PublicKey publicKey) {
+        try {
+            return Jwts.parser()
+                    .setSigningKey(publicKey)
+                    .parseClaimsJws(idToken)
+                    .getBody();
+        } catch (ExpiredJwtException e) {
+            throw new TokenExpiredException("Apple OAuth 로그인 중 Identity Token 유효기간이 만료됐습니다.");
+        } catch (UnsupportedJwtException | MalformedJwtException | SignatureException | IllegalArgumentException e) {
+            throw new InvalidTokenException("Apple OAuth Identity Token 값이 올바르지 않습니다.");
+        }
+    }
+}
