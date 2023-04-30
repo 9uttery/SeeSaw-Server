183b2a93464b396e3e77ec64bbb96dfb955e6900
mingeun0507 <tqdlfkg@naver.com>
2023-04-30 20:28:56 +0900

    Feat(#4-login): Apple로부터 공개키를 받아오는 FeignClient 구현

---
+++ src/main/java/com/_8attery/seesaw/auth/apple/AppleClient.java
@@ -0,0 +1,13 @@
+package com._8attery.seesaw.auth.apple;
+
+
+import com._8attery.seesaw.dto.auth.apple.ApplePublicKeys;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@FeignClient(name = "apple-public-key-client", url = "https://appleid.apple.com/auth")
+public interface AppleClient {
+
+    @GetMapping("/keys")
+    ApplePublicKeys getApplePublicKeys();
+}
