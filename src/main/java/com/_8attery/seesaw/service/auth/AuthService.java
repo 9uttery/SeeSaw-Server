57c1a826e8bad009068ed725d917959c3e1ecc9b
mingeun0507 <tqdlfkg@naver.com>
2023-05-01 23:58:23 +0900

    Fix(#4-login): 소셜 로그인 시 이메일이 이미 있는 경우도 정상적으로 처리하도록 변경

--- src/main/java/com/_8attery/seesaw/service/auth/AuthService.java
+++ src/main/java/com/_8attery/seesaw/service/auth/AuthService.java
@@ -1,6 +1,8 @@
 package com._8attery.seesaw.service.auth;
 
+import com._8attery.seesaw.domain.user.User;
 import com._8attery.seesaw.dto.auth.request.LoginRequestDto;
+import com._8attery.seesaw.dto.auth.request.RefreshTokenRequestDto;
 import com._8attery.seesaw.dto.auth.response.LoginResponseDto;
 
 public interface AuthService {
@@ -8,8 +10,13 @@ public interface AuthService {
     // 객체가 이미 존재하는지 판단하는 메소드
     boolean userExistsByEmail(String email);
 
+    // 객체가 존재하는지 판단하고 존재하면 객체를 반환하는 메소드
+    User findUserById(Long userId);
+
     // 비즈니스 로직
     LoginResponseDto appleLogin(LoginRequestDto loginRequestDto);
+
+    LoginResponseDto regenerateAccessToken(RefreshTokenRequestDto refreshTokenRequestDto);
 
 //    LoginResponseDto kakaoLogin(LoginRequestDto loginRequestDto);
 }
