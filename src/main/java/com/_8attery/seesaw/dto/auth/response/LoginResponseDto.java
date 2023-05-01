6030486a60b13e7b88bce4ce0a55f1a08e41f1a6
mingeun0507 <tqdlfkg@naver.com>
2023-05-01 18:20:20 +0900

    Style: LoginResponse JSON에 맞게 이름 수정

--- src/main/java/com/_8attery/seesaw/dto/auth/response/LoginResponseDto.java
+++ src/main/java/com/_8attery/seesaw/dto/auth/response/LoginResponseDto.java
@@ -1,5 +1,6 @@
 package com._8attery.seesaw.dto.auth.response;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
@@ -8,7 +9,11 @@ import lombok.NoArgsConstructor;
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class LoginResponseDto {
+
+    @JsonProperty("access_token")
     private String accessToken;
+
+    @JsonProperty("refresh_token")
     private String refreshToken;
 
     @Builder
