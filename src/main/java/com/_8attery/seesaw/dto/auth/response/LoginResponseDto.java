9c0af6079eea425dde18dd4821ac32f39faabb4b
mingeun0507 <tqdlfkg@naver.com>
2023-05-01 11:45:26 +0900

    Feat(#4-login): 로그인 시 사용할 Request, Response DTO 구현

---
+++ src/main/java/com/_8attery/seesaw/dto/auth/response/LoginResponseDto.java
@@ -0,0 +1,20 @@
+package com._8attery.seesaw.dto.auth.response;
+
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class LoginResponseDto {
+    private String accessToken;
+    private String refreshToken;
+
+    @Builder
+    public LoginResponseDto(String accessToken, String refreshToken) {
+        this.accessToken = accessToken;
+        this.refreshToken = refreshToken;
+    }
+
+}
