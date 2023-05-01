750b4d5c8f1fcf54acd01687ae632ad9aaeab342
김민근 <tqdlfkg@naver.com>
2023-05-02 00:06:01 +0900

    Fix(#4-login): import 에러 수정

--- src/main/java/com/_8attery/seesaw/domain/user/UserRepository.java
+++ src/main/java/com/_8attery/seesaw/domain/user/UserRepository.java
@@ -2,6 +2,8 @@ package com._8attery.seesaw.domain.user;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, Long> {
     boolean existsByEmail(String email);
     
