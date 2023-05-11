d38dec3126cf2c7be775edf7105b0a5e1d0d6828
hojeong2747 <hojeong2747@gmail.com>
2023-05-11 14:07:44 +0900

    Fix(#21-value): 사용자 가치 조회 API

--- src/main/java/com/_8attery/seesaw/controller/value/ValueController.java
+++ src/main/java/com/_8attery/seesaw/controller/value/ValueController.java
@@ -2,6 +2,7 @@ package com._8attery.seesaw.controller.value;
 
 import com._8attery.seesaw.domain.user.account.UserAccount;
 import com._8attery.seesaw.dto.api.request.ValueReq;
+import com._8attery.seesaw.dto.api.response.ValueRes;
 import com._8attery.seesaw.exception.BaseException;
 import com._8attery.seesaw.exception.BaseResponse;
 import com._8attery.seesaw.service.user.UserService;
@@ -9,6 +10,7 @@ import com._8attery.seesaw.service.value.ValueService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -34,7 +36,16 @@ public class ValueController {
         } catch(BaseException exception){
             return new BaseResponse<>((exception.getStatus()));
         }
+    }
 
+    @GetMapping("/api/value")
+    public BaseResponse<ValueRes> getValues(@AuthenticationPrincipal UserAccount userAccount) throws BaseException {
+
+        Long userId = userService.resolveUserById(userAccount.getUserId()).getId();
+
+        ValueRes res = valueService.get3Values(userId);
+
+        return new BaseResponse<>(res);
     }
 
 }
