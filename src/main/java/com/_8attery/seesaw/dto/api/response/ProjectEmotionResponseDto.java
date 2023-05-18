a8703021816ee507c85424c0a960b325e3982328
mingeun0507 <tqdlfkg@naver.com>
2023-05-18 18:13:52 +0900

    Fix: Json 컨벤션으로 수정

--- src/main/java/com/_8attery/seesaw/dto/api/response/ProjectEmotionResponseDto.java
+++ src/main/java/com/_8attery/seesaw/dto/api/response/ProjectEmotionResponseDto.java
@@ -1,6 +1,7 @@
 package com._8attery.seesaw.dto.api.response;
 
 import com._8attery.seesaw.domain.project_emotion.ProjectEmotion;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
@@ -9,10 +10,15 @@ import lombok.NoArgsConstructor;
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class ProjectEmotionResponseDto {
+    @JsonProperty("like_cnt")
     private Integer likeCnt;
+    @JsonProperty("nice_cnt")
     private Integer niceCnt;
+    @JsonProperty("idk_cnt")
     private Integer idkCnt;
+    @JsonProperty("angry_cnt")
     private Integer angryCnt;
+    @JsonProperty("sad_cnt")
     private Integer sadCnt;
 
     @Builder
