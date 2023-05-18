83ed10dd00658e38983f6804822363ac4de818e4
mingeun0507 <tqdlfkg@naver.com>
2023-05-18 23:25:40 +0900

    Feat(#63-project): ProjectRecord 관련 조회 및 수정 메소드 구현

---
+++ src/main/java/com/_8attery/seesaw/domain/project_record/ProjectRecordRepository.java
@@ -0,0 +1,7 @@
+package com._8attery.seesaw.domain.project_record;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProjectRecordRepository extends JpaRepository<ProjectRecord, Long> {
+
+}
