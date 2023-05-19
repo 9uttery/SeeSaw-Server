0254b1166015235183987573d0bc843952261204
mingeun0507 <tqdlfkg@naver.com>
2023-05-20 03:15:08 +0900

    Fix(#63-project): 도메인 변경에 따른 수정

--- src/main/java/com/_8attery/seesaw/domain/project_record/ProjectRecordRepositoryCustomImpl.java
+++ src/main/java/com/_8attery/seesaw/domain/project_record/ProjectRecordRepositoryCustomImpl.java
@@ -1,13 +1,14 @@
 package com._8attery.seesaw.domain.project_record;
 
 import com._8attery.seesaw.dto.api.response.ProjectRecordResponseDto;
+import com.querydsl.core.types.Projections;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
-import java.util.stream.Collectors;
 
+import static com._8attery.seesaw.domain.project_question.QProjectQuestion.projectQuestion;
 import static com._8attery.seesaw.domain.project_record.QProjectRecord.projectRecord;
 
 @RequiredArgsConstructor
@@ -18,14 +19,17 @@ public class ProjectRecordRepositoryCustomImpl implements ProjectRecordRepositor
 
     @Override
     public List<ProjectRecordResponseDto> findAllRecordsByProjectId(Long projectId) {
-
-        return jpaQueryFactory
-                .select(projectRecord)
+        return jpaQueryFactory.select(Projections.constructor(
+                        ProjectRecordResponseDto.class,
+                        projectRecord.id,
+                        projectRecord.createdAt,
+                        projectQuestion.contents,
+                        projectRecord.contents)
+                )
                 .from(projectRecord)
+                .leftJoin(projectQuestion).on(projectRecord.projectQuestion.id.eq(projectQuestion.id))
+                .fetchJoin()
                 .where(projectRecord.project.id.eq(projectId))
-                .fetch()
-                .stream()
-                .map(ProjectRecordResponseDto::from)
-                .collect(Collectors.toList());
+                .fetch();
     }
 }
