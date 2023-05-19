d30f65844432db75be49f4c3b772e7b217180f79
mingeun0507 <tqdlfkg@naver.com>
2023-05-20 03:15:08 +0900

    Fix(#63-project): 도메인 변경에 따른 수정

---
+++ src/main/java/com/_8attery/seesaw/domain/project_question/ProjectQuestionRepository.java
@@ -0,0 +1,6 @@
+package com._8attery.seesaw.domain.project_question;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProjectQuestionRepository extends JpaRepository<ProjectQuestion, Long> {
+}
