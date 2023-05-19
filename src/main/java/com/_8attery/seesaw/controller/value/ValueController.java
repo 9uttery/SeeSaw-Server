2a087cb9c0dff4b1bb8d26c7cbe053f245ccfced
hojeong2747 <hojeong2747@gmail.com>
2023-05-20 00:01:55 +0900

    Feat(#21-value): 가치에 맞는 프로젝트, 고속충전 조회 API

--- src/main/java/com/_8attery/seesaw/controller/value/ValueController.java
+++ src/main/java/com/_8attery/seesaw/controller/value/ValueController.java
@@ -1,7 +1,10 @@
 package com._8attery.seesaw.controller.value;
 
 import com._8attery.seesaw.domain.user.account.UserAccount;
+import com._8attery.seesaw.dto.api.request.ProjectRequestDto;
 import com._8attery.seesaw.dto.api.request.ValueRequestDto;
+import com._8attery.seesaw.dto.api.response.ProjectResponseDto;
+import com._8attery.seesaw.dto.api.response.ValueInfoResponseDto;
 import com._8attery.seesaw.dto.api.response.ValueResponseDto;
 import com._8attery.seesaw.exception.BaseException;
 import com._8attery.seesaw.exception.BaseResponse;
@@ -10,12 +13,12 @@ import com._8attery.seesaw.service.value.ValueService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+
+import static com._8attery.seesaw.exception.BaseResponseStatus.DATABASE_ERROR;
+import static com._8attery.seesaw.exception.BaseResponseStatus.USERS_FAILED_POST_ID;
 
 @Slf4j
 //@Api(tags = {"3. Value"})
@@ -27,6 +30,7 @@ public class ValueController {
 
     private final ValueService valueService;
 
+    // 사용자 가치 선택
     @PostMapping("/api/value")
     public BaseResponse<Integer> setValues(@RequestBody ValueRequestDto req, @AuthenticationPrincipal UserAccount userAccount) throws BaseException {
         Long userId = userService.resolveUserById(userAccount.getUserId()).getId();
@@ -39,12 +43,27 @@ public class ValueController {
         }
     }
 
+    // 사용자 가치 조회
     @GetMapping("/api/value")
     public BaseResponse<List<ValueResponseDto>> getValues(@AuthenticationPrincipal UserAccount userAccount) throws BaseException {
         Long userId = userService.resolveUserById(userAccount.getUserId()).getId();
 
         List<ValueResponseDto> res = valueService.get3Values(userId);
         return new BaseResponse<>(res);
+    }
+
+    // 가치에 맞는 프로젝트, 고속 충전 조회
+    @GetMapping("/api/value/{valueId}")
+    public BaseResponse<ValueInfoResponseDto> getValueInfo(@PathVariable("valueId") Long valueId, @AuthenticationPrincipal UserAccount userAccount) throws BaseException {
+        Long userId = userService.resolveUserById(userAccount.getUserId()).getId();
+
+        try {
+            ValueInfoResponseDto res = valueService.getUserValueInfo(userId, valueId);
+
+            return new BaseResponse<>(res);
+        } catch(BaseException exception){
+            return new BaseResponse<>((exception.getStatus()));
+        }
     }
 
 }
--- src/main/java/com/_8attery/seesaw/service/value/ValueService.java
+++ src/main/java/com/_8attery/seesaw/service/value/ValueService.java
@@ -1,6 +1,8 @@
 package com._8attery.seesaw.service.value;
 
+import com._8attery.seesaw.domain.project.Project;
 import com._8attery.seesaw.domain.value.ValueRepository;
+import com._8attery.seesaw.dto.api.response.ValueInfoResponseDto;
 import com._8attery.seesaw.dto.api.response.ValueResponseDto;
 import com._8attery.seesaw.exception.BaseException;
 import com._8attery.seesaw.exception.BaseResponseStatus;
@@ -8,9 +10,12 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.time.Duration;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
+
+import static com._8attery.seesaw.exception.BaseResponseStatus.DATABASE_ERROR;
 
 @Service
 @Transactional(readOnly = true)
@@ -19,6 +24,7 @@ public class ValueService {
 
     private final ValueRepository valueRepository;
 
+    // 사용자 가치 선택
     @Transactional
     public int set3Values(List<String> values, Long userId) throws BaseException {
         int count = checkValues(userId);
@@ -45,6 +51,7 @@ public class ValueService {
         return count;
     }
 
+    // 사용자 가치 조회
     public List<ValueResponseDto> get3Values(Long userId) {
         List<Object[]> values = valueRepository.getUser3Values(userId);
 
@@ -57,5 +64,58 @@ public class ValueService {
         }
 
         return list;
+    }
+
+    // 가치에 맞는 프로젝트, 고속 충전 조회
+    public ValueInfoResponseDto getUserValueInfo(Long userId, Long valueId) throws BaseException {
+        try {
+            ValueInfoResponseDto res = new ValueInfoResponseDto();
+            List<ValueInfoResponseDto.ValueProject> projectList = new ArrayList<>();
+
+            // 1. 가치에 맞는 프로젝트 목록 가져오기 (프로젝트 이름, 시작/끝 날짜 필요)
+            List<Project> findProjects = valueRepository.findValueProject(valueId);
+
+            // 2. 가져온 프로젝트 목록 개수만큼 반복문 돌면서 projectList 에 프로젝트 이름, 진행률 설정
+            for (Project pro : findProjects) {
+                ValueInfoResponseDto.ValueProject dto = new ValueInfoResponseDto.ValueProject();
+                dto.setProjectName(pro.getProjectName());
+
+                LocalDateTime startedAt = pro.getStartedAt();
+                LocalDateTime endedAt = pro.getEndedAt();
+                Double rate = calculateProgressPercentage(startedAt, endedAt);
+                dto.setProgressRate(rate);
+
+                projectList.add(dto);
+            }
+
+            // 3. res 에 projectList, 진행률 설정
+            res.setProjectList(projectList);
+            res.setCount(valueRepository.findValueCharge(valueId));
+
+            return res;
+
+        } catch (Exception exception) {
+            exception.printStackTrace();
+            throw new BaseException(DATABASE_ERROR);
+        }
+    }
+
+    // 프로젝트 진행률 계산
+    public static double calculateProgressPercentage(LocalDateTime startDate, LocalDateTime endDate) {
+        LocalDateTime now = LocalDateTime.now();
+
+        Duration fullPeriod = Duration.between(startDate, endDate);
+        Duration progressedPeriod = Duration.between(startDate, now);
+
+        long fullPeriodSeconds = fullPeriod.getSeconds();
+        long progressedPeriodSeconds = progressedPeriod.getSeconds();
+
+        if (fullPeriodSeconds == 0) {
+            return 0.0; // Handle the case when the full period is zero to avoid division by zero error
+        }
+
+        double progressPercentage = (double) progressedPeriodSeconds / fullPeriodSeconds * 100;
+
+        return Math.round(progressPercentage * 10) / 10.0; // Round to one decimal place
     }
 }
