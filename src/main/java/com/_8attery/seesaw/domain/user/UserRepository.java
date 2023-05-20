49f64e69edd6083585e67b96a7844a676c61bc56
hojeong2747 <hojeong2747@gmail.com>
2023-05-20 18:33:11 +0900

    Feat(#104-mypage): 닉네임 변경 API

--- src/main/java/com/_8attery/seesaw/domain/user/UserRepository.java
+++ src/main/java/com/_8attery/seesaw/domain/user/UserRepository.java
@@ -1,6 +1,9 @@
 package com._8attery.seesaw.domain.user;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.Optional;
 
@@ -8,4 +11,12 @@ public interface UserRepository extends JpaRepository<User, Long> {
     boolean existsByEmail(String email);
     
     Optional<User> findByEmail(String email);
+
+    @Modifying(clearAutomatically = true)
+    @Query(value = "update ss_user set nick_name=:nickName where user_id=:userId", nativeQuery = true)
+    void updateUserNickname(@Param("userId") Long userId, @Param("nickName") String nickName);
+
+    @Query(value = "select nick_name from ss_user where user_id=:userId", nativeQuery = true)
+    String findUserNickname(@Param("userId") Long userId);
+
 }
--- src/main/java/com/_8attery/seesaw/service/user/UserServiceImpl.java
+++ src/main/java/com/_8attery/seesaw/service/user/UserServiceImpl.java
@@ -2,10 +2,16 @@ package com._8attery.seesaw.service.user;
 
 import com._8attery.seesaw.domain.user.User;
 import com._8attery.seesaw.domain.user.UserRepository;
+import com._8attery.seesaw.exception.BaseException;
 import com._8attery.seesaw.exception.custom.ResourceNotFoundException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDateTime;
+
+import static com._8attery.seesaw.exception.BaseResponseStatus.DATABASE_ERROR;
 
 @RequiredArgsConstructor
 @Slf4j
@@ -18,5 +24,29 @@ public class UserServiceImpl implements UserService {
 
         return userRepository.findById(userId)
                 .orElseThrow(() -> new ResourceNotFoundException("해당 id를 가진 사용자를 찾을 수 없습니다."));
+    }
+
+    @Override
+    @Transactional
+    public void updateNickname(Long userId, String nickName) throws BaseException {
+        try {
+            userRepository.updateUserNickname(userId, nickName);
+
+        } catch (Exception exception) {
+            exception.printStackTrace();
+            throw new BaseException(DATABASE_ERROR);
+        }
+    }
+
+    @Override
+    public String getUserNickname(Long userId) throws BaseException {
+        try {
+
+            return userRepository.findUserNickname(userId);
+
+        } catch (Exception exception) {
+            exception.printStackTrace();
+            throw new BaseException(DATABASE_ERROR);
+        }
     }
 }
