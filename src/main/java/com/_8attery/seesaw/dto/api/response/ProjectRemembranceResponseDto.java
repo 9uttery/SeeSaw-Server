f4b03a9f583cf6462241ee98c3bf0ee51829290a
mingeun0507 <tqdlfkg@naver.com>
2023-05-22 22:41:59 +0900

    Fix(#63-project): 프로젝트 회고록 API 응답 DTO 수정

--- src/main/java/com/_8attery/seesaw/dto/api/response/ProjectRemembranceResponseDto.java
+++ src/main/java/com/_8attery/seesaw/dto/api/response/ProjectRemembranceResponseDto.java
@@ -1,5 +1,6 @@
 package com._8attery.seesaw.dto.api.response;
 
+import com._8attery.seesaw.domain.project_remembrance.RemembranceType;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.*;
 
@@ -7,12 +8,22 @@ import java.util.List;
 
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-@AllArgsConstructor
 public class ProjectRemembranceResponseDto {
     @JsonProperty("remembrance_id")
     private Long remembranceId;
-    @JsonProperty("project_id")
+
+    @JsonProperty("type")
+    private RemembranceType type;
+
+    @JsonProperty("qna_list")
     private List<QnaDto> qnaList;
+
+    @Builder
+    public ProjectRemembranceResponseDto(Long remembranceId, RemembranceType remembranceType, List<QnaDto> qnaList) {
+        this.remembranceId = remembranceId;
+        this.type = remembranceType;
+        this.qnaList = qnaList;
+    }
 
     @Data
     @NoArgsConstructor
@@ -20,7 +31,9 @@ public class ProjectRemembranceResponseDto {
     public static class QnaDto {
         @JsonProperty("qna_id")
         private Long qnaId;
+        @JsonProperty("question")
         private String question;
+        @JsonProperty("answer")
         private String answer;
     }
 }
