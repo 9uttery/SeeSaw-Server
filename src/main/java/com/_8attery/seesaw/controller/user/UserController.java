e35ef340585665f036a63d2d6dfe86f48479bb8b
mingeun0507 <tqdlfkg@naver.com>
2023-05-23 22:16:49 +0900

    Feat(#63-project): 회원 탈퇴 API 구현

--- src/main/java/com/_8attery/seesaw/controller/user/UserController.java
+++ src/main/java/com/_8attery/seesaw/controller/user/UserController.java
@@ -7,12 +7,13 @@ import com._8attery.seesaw.exception.BaseException;
 import com._8attery.seesaw.exception.BaseResponse;
 import com._8attery.seesaw.service.user.UserService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 @RequiredArgsConstructor
 @RestController
-//@RequestMapping("/api/user")
+@RequestMapping("/api/user")
 public class UserController {
     private final UserService userService;
 
@@ -22,7 +23,7 @@ public class UserController {
 //    }
 
     // 닉네임 조회
-    @GetMapping("/api/user")
+    @GetMapping
     public BaseResponse<String> getUser (@AuthenticationPrincipal UserAccount userAccount) throws BaseException {
         Long userId = userService.resolveUserById(userAccount.getUserId()).getId();
 
@@ -37,7 +38,7 @@ public class UserController {
 
 
     // 닉네임 수정
-    @PutMapping("/api/user")
+    @PutMapping
     public BaseResponse<String> updateUser (@RequestBody NicknameRequestDto req, @AuthenticationPrincipal UserAccount userAccount) throws BaseException {
         Long userId = userService.resolveUserById(userAccount.getUserId()).getId();
 
@@ -53,12 +54,18 @@ public class UserController {
     }
 
     // 회원 탈퇴 화면 - 함께 한 일수, 가치 목록 조회 API
-    @GetMapping("/api/user/history")
-    public BaseResponse<UserHistoryResponseDto> getHistory (@AuthenticationPrincipal UserAccount userAccount) throws BaseException {
+    @GetMapping("/history")
+    public BaseResponse<UserHistoryResponseDto> getHistory(@AuthenticationPrincipal UserAccount userAccount) throws BaseException {
         Long userId = userService.resolveUserById(userAccount.getUserId()).getId();
 
         UserHistoryResponseDto res = userService.getUserHistory(userId);
 
         return new BaseResponse<>(res);
+    }
+
+    @DeleteMapping()
+    public ResponseEntity<Long> deleteUser(@AuthenticationPrincipal UserAccount userAccount) {
+        userService.deleteUser(userAccount.getUserId());
+        return ResponseEntity.ok(userAccount.getUserId());
     }
 }
