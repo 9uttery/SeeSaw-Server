357d57e81d76a8e2bc63b77c76f19e1fe9ea505f
mingeun0507 <tqdlfkg@naver.com>
2023-05-25 02:58:35 +0900

    Fix(#63-project): 접근 제어자 수정

--- src/main/java/com/_8attery/seesaw/dto/api/response/InitialProjectReportResponseDto.java
+++ src/main/java/com/_8attery/seesaw/dto/api/response/InitialProjectReportResponseDto.java
@@ -1,11 +1,10 @@
 package com._8attery.seesaw.dto.api.response;
 
 import com._8attery.seesaw.domain.project_emotion.Emotion;
-import lombok.AccessLevel;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import com._8attery.seesaw.domain.project_remembrance.RemembranceType;
+import lombok.*;
 
+import javax.persistence.Enumerated;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.List;
@@ -14,16 +13,17 @@ import java.util.List;
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class InitialProjectReportResponseDto {
 
-    ProjectReportInfoDto projectReportInfoDto;
-    Emotion emotion1;
-    Emotion emotion2;
-    ProjectMiddleFinalQnaDto projectMiddleFinalQnaDto;
-    Long todayAnswerCount;
-    Long recordAnswerCount;
-    List<SimpleRecordInfo> simpleRecordInfo;
+    private ProjectReportInfoDto projectReportInfoDto;
+    private Emotion emotion1;
+    private Emotion emotion2;
+    private ProjectMiddleFinalQnaDto projectMiddleFinalQnaDto;
+    private Long todayAnswerCount;
+    private Long recordAnswerCount;
+    private List<SimpleRecordInfo> simpleRecordInfo;
+    private List<AnswerContentDto> answerContentList;
 
     @Builder
-    public InitialProjectReportResponseDto(ProjectReportInfoDto projectReportInfoDto, Emotion emotion1, Emotion emotion2, ProjectMiddleFinalQnaDto projectMiddleFinalQnaDto, Long todayAnswerCount, Long recordAnswerCount, List<SimpleRecordInfo> simpleRecordInfo) {
+    public InitialProjectReportResponseDto(ProjectReportInfoDto projectReportInfoDto, Emotion emotion1, Emotion emotion2, ProjectMiddleFinalQnaDto projectMiddleFinalQnaDto, Long todayAnswerCount, Long recordAnswerCount, List<SimpleRecordInfo> simpleRecordInfo, List<AnswerContentDto> answerContentList) {
         this.projectReportInfoDto = projectReportInfoDto;
         this.emotion1 = emotion1;
         this.emotion2 = emotion2;
@@ -31,19 +31,31 @@ public class InitialProjectReportResponseDto {
         this.todayAnswerCount = todayAnswerCount;
         this.recordAnswerCount = recordAnswerCount;
         this.simpleRecordInfo = simpleRecordInfo;
+        this.answerContentList = answerContentList;
     }
 
     @Data
     @NoArgsConstructor
     public static class SimpleRecordInfo {
         private LocalDate createdAt;
-        private String recordContent;
+        private String content;
 
         @Builder
-        public SimpleRecordInfo(LocalDateTime createdAt, String recordContent) {
+        public SimpleRecordInfo(LocalDateTime createdAt, String content) {
             this.createdAt = createdAt.toLocalDate();
-            this.recordContent = recordContent;
+            this.content = content;
         }
+    }
+
+    @Data
+    @NoArgsConstructor
+    @AllArgsConstructor
+    @Builder
+    public static class AnswerContentDto {
+        @Enumerated(javax.persistence.EnumType.STRING)
+        private RemembranceType remembranceType;
+        private Long choice;
+        private String content;
     }
 
 }
